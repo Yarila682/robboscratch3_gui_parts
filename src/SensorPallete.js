e61df05900be35fb23746524ffe3b072450aa49f
yarila682 <yarila682@yandex.ru>
2018-02-25 22:24:47 +0300

    robot and laboratory special sensors added

--- src/SensorPallete.js
+++ src/SensorPallete.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './SensorPallete.css';
 import SensorComponent from './SensorComponent';
+import SensorDataBlockComponent  from './SensorDataBlockComponent'
 import SensorChooseWindowComponent from './SensorChooseWindowComponent';
 
 
@@ -37,30 +38,45 @@ class SensorPallete extends Component {
 
               <div id="robot-sensors-tittle" className="sensor-panel-tittle"> Robot   </div>
 
+                <SensorDataBlockComponent key={this.props.robot_special_sensors[0].sensor_id} sensorId={this.props.robot_special_sensors[0].sensor_id}
+                                   deviceName={this.props.robot_special_sensors[0].sensor_device_name} sensorType={this.props.robot_special_sensors[0].sensor_type}
+                                   sensorFieldText={this.props.robot_special_sensors[0].sensor_field_text} />
+
+                <SensorDataBlockComponent key={this.props.robot_special_sensors[1].sensor_id} sensorId={this.props.robot_special_sensors[1].sensor_id}
+                                                      deviceName={this.props.robot_special_sensors[1].sensor_device_name} sensorType={this.props.robot_special_sensors[1].sensor_type}
+                                                      sensorFieldText={this.props.robot_special_sensors[1].sensor_field_text} />
+
             {
 
-             this.props.robot_sensors.map((sensor, index) =>
 
-                  {
+                  this.props.robot_sensors.map((sensor, index) =>
 
-                    let sensorPictureUrl = `/assets/16/${sensor.sensor_device_name}_sensor_${sensor.sensor_name}.png`;
+                     {
 
-
-
-                    return   <SensorComponent key={index} index={index} sensorId={sensor.sensor_id} isSensorVersionNew={sensor.is_sensor_version_new}
-                                sensorPictureUrl={sensorPictureUrl}
-                                deviceName={sensor.sensor_device_name} sensorType={sensor.sensor_type}
-                                sensorFieldText={sensor.sensor_field_text}/>
+                       let sensorPictureUrl = `/assets/16/${sensor.sensor_device_name}_sensor_${sensor.sensor_name}.png`;
 
 
 
-                  }
+                       return   <SensorComponent key={index} index={index} sensorId={sensor.sensor_id} isSensorVersionNew={sensor.is_sensor_version_new}
+                                   sensorPictureUrl={sensorPictureUrl}
+                                   deviceName={sensor.sensor_device_name} sensorType={sensor.sensor_type}
+                                   sensorFieldText={sensor.sensor_field_text}/>
 
-              )
+
+
+                     }
+
+                 )
+
 
 
 
             }
+
+
+            <SensorDataBlockComponent key={this.props.robot_special_sensors[2].sensor_id} sensorId={this.props.robot_special_sensors[2].sensor_id}
+                               deviceName={this.props.robot_special_sensors[2].sensor_device_name} sensorType={this.props.robot_special_sensors[2].sensor_type}
+                               sensorFieldText={this.props.robot_special_sensors[2].sensor_field_text} />
 
           </div>
 
@@ -70,26 +86,62 @@ class SensorPallete extends Component {
 
             {
 
-             this.props.lab_external_sensors.map((sensor, index) =>
 
-                  {
+              (() => {
 
-
-
-                    let sensorPictureUrl = `/assets/16/${sensor.sensor_device_name}_sensor_${sensor.sensor_name}.png`;
+                let components = [];
 
 
+                                        this.props.lab_special_sensors.map((sensor, index) =>
 
-                    return   <SensorComponent key={index} index={index} sensorId={sensor.sensor_id} isSensorVersionNew={sensor.is_sensor_version_new}
-                                sensorPictureUrl={sensorPictureUrl}
-                                deviceName={sensor.sensor_device_name} sensorType={sensor.sensor_type}
-                                sensorFieldText={sensor.sensor_field_text}/>
+                                                    {
+
+                                                  components.push(<SensorDataBlockComponent key={sensor.sensor_id} sensorId={sensor.sensor_id}
+                                                                      deviceName={sensor.sensor_device_name} sensorType={sensor.sensor_type}
+                                                                      sensorFieldText={sensor.sensor_field_text} />
+
+
+                                                                  );
 
 
 
-                  }
+                                                     }
 
-              )
+                                                                              );
+
+
+                                          this.props.lab_external_sensors.map((sensor, index) =>
+
+                                             {
+
+
+
+                                               let sensorPictureUrl = `/assets/16/${sensor.sensor_device_name}_sensor_${sensor.sensor_name}.png`;
+
+
+
+                                               components.push(<SensorComponent key={index} index={index} sensorId={sensor.sensor_id} isSensorVersionNew={sensor.is_sensor_version_new}
+                                                           sensorPictureUrl={sensorPictureUrl}
+                                                           deviceName={sensor.sensor_device_name} sensorType={sensor.sensor_type}
+                                                           sensorFieldText={sensor.sensor_field_text}/>);
+
+
+
+                                             }
+
+                                                                              )
+
+
+
+                return (
+
+                        components
+
+                        );
+                      }
+              )()
+
+
 
 
 
@@ -116,6 +168,8 @@ const mapStateToProps =  state => ({
 
       robot_sensors: state.robot_sensors,
       lab_external_sensors:  state.lab_external_sensors,
+      robot_special_sensors: state.robot_special_sensors,
+      lab_special_sensors:  state.lab_special_sensors,
       sensors_choose_window: state.sensors_choose_window
   });
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,8 @@ import { combineReducers } from 'redux';
 
 import robot_sensors from './robot_sensors';
 import lab_external_sensors from './lab_external_sensors';
+import robot_special_sensors  from './robot_special_sensors'
+import lab_special_sensors  from './lab_special_sensors'
 import trash_reducer from './trash_reducer';
 import sensors_choose_window from './sensors_choose_window';
 
@@ -10,6 +12,8 @@ export default combineReducers({
 
   robot_sensors:robot_sensors,
   lab_external_sensors:lab_external_sensors,
+  robot_special_sensors: robot_special_sensors,
+  lab_special_sensors: lab_special_sensors,
   trash_reducer:trash_reducer,
   sensors_choose_window:sensors_choose_window
 
